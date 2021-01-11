dee8e4c5ed91fdffc09f66768f5633d3661a3696
LAndresZuMoc <62224421+LAndresZuMoc@users.noreply.github.com>
2021-01-11 04:24:17 -0600

    Add files via upload

---
+++ ScryFallText/search.php
@@ -0,0 +1,46 @@
+<?php 
+if (!empty($_GET["cardName"])){
+    $scryfall_Url="https://api.scryfall.com/cards/search?order=name&q=".($_GET["cardName"]);
+    $scryfall_json = file_get_contents($scryfall_Url);
+    $scryfall_array = json_decode($scryfall_json,true);
+    //$scryfall_array["name"]["mana_cost"]["oracle_text"];
+    
+    
+    
+}
+?>
+<HTML>
+   <head>
+     <title>Search</title>
+   </head>
+   <body style="background-color:black">
+   <font color="white">
+   <center>
+      <form action="">
+       <input type="text" name="cardName">
+       <button type="submit">Search</button>
+       <br>
+       <?php 
+       
+       foreach ($scryfall_array["data"] as $image){
+           
+          echo "Card Name: ". $image["name"];
+           
+          echo "<br>";
+          echo '<img src="'.$image['image_uris']['small'].'"alt=""./>';
+          echo "<br>";
+          echo "Price: ". $image["prices"]["usd"];
+          echo "<br>";
+           
+           
+           
+       }
+       
+       ?>
+      
+         
+      </form>
+   </center>
+</font>
+   </body>
+</HTML>
